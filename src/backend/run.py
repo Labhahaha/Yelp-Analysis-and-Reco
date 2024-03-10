9f28485679c135a2ba10467b1ebb5024c5c5185b
lab <1302527662@qq.com>
2024-03-10 15:43:24 +0800

    开启flask远程访问

--- src/backend/run.py
+++ src/backend/run.py
@@ -2,4 +2,4 @@ from app import create_app
 from config import config
 if __name__ == "__main__":
     app=create_app(config)
-    app.run(debug=True)
+    app.run(host="0.0.0.0",port=5000,debug=True)
