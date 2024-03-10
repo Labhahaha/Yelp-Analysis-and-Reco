c7256a33205dcf61c5b269311b874a7b1ed40b3e
zzc <2278860740@qq.com>
2024-03-10 16:21:57 +0800

    增加了打卡分析功能

---
+++ src/backend/app/DataAnalyse/Checkin.py
@@ -0,0 +1,46 @@
+from flask import Blueprint
+from sqlalchemy import text
+from .SQLSession import get_session,toJSON,ToDataFrame
+# 创建蓝图
+checkin_blue= Blueprint('checkin', __name__,)
+
+
+# 统计每年的打卡次数
+@checkin_blue.route('/checkin_count_by_year')
+def checkin_count_by_year():
+    with get_session() as session:
+        query = text("select * from checkin_count_by_year")
+        res = session.execute(query)
+        json_res = toJSON(res)
+        return json_res
+
+
+# 统计24小时每小时打卡次数
+@checkin_blue.route('/checkin_count_by_hour')
+def checkin_count_by_hour():
+    with get_session() as session:
+        query = text("select * from checkin_count_by_hour")
+        res = session.execute(query)
+        json_res = toJSON(res)
+        return json_res
+
+
+# 统计最喜欢打卡的城市
+@checkin_blue.route('/city_with_most_checkin')
+def city_with_most_checkin():
+    with get_session() as session:
+        query = text("select * from city_with_most_checkin")
+        res = session.execute(query)
+        json_res = toJSON(res)
+        return json_res
+
+
+# 全部商家的打卡排行榜
+@checkin_blue.route('/business_order_by_checkin_count')
+def business_order_by_checkin_count():
+    with get_session() as session:
+        query = text("select * from business_order_by_checkin_count")
+        res = session.execute(query)
+        json_res = toJSON(res)
+        return json_res
+
