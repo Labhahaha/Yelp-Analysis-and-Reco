98d68d77607adf1ba1525cf1690b65267dc20299
weeadd <scu_wj@qq.com>
2024-03-10 16:16:55 +0800

    更新DataAnalysis-Users相关函数及路由

--- src/backend/app/DataAnalyse/__init__.py
+++ src/backend/app/DataAnalyse/__init__.py
@@ -1,4 +1,5 @@
 from .Business import business_blue
+from .Users import users_blue
 from .SQLSession import db_init
 
 
--- src/backend/app/__init__.py
+++ src/backend/app/__init__.py
@@ -1,6 +1,6 @@
 from flask import Flask
 from flask_cors import CORS
-from .DataAnalyse import business_blue,db_init
+from .DataAnalyse import business_blue,users_blue,db_init
 
 def create_app(config):
     #ÊµÀý»¯app
@@ -8,7 +8,10 @@ def create_app(config):
     CORS(app, supports_credentials=True)
 
     db_init(config.DATABASE_URL)
+
     app.register_blueprint(business_blue, url_prefix='/business')
+
+    app.register_blueprint(users_blue, url_prefix='/users')
 
     return app
 
