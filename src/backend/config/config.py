eb0da9c10ee087048e078506ed3eaaf3c41623a5
lab <1302527662@qq.com>
2024-03-11 14:07:20 +0800

    更新获取动态IP进行数据库连接

--- src/backend/config/config.py
+++ src/backend/config/config.py
@@ -1,4 +1,7 @@
+import socket
+# 动态获取IP地址
+Local_IP = socket.getaddrinfo('LAPTOP-SISP97O2',None)[0][4][0]
 # 数据库连接配置
-DATABASE_URL = 'mysql+pymysql://root:123456@172.16.0.247:3306/yelp'
+DATABASE_URL = f'mysql+pymysql://root:123456@{Local_IP}:3306/yelp'
 TRACK_MODIFICATIONS = False
 
