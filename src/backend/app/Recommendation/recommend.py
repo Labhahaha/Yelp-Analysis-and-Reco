74e8fe519af2662e3235f1da81adb8ba594353a9
zzc <2278860740@qq.com>
2024-03-11 17:25:28 +0800

    增加了得到用户与一组商家的距离功能

--- src/backend/app/Recommendation/recommend.py
+++ src/backend/app/Recommendation/recommend.py
@@ -2,6 +2,18 @@ from flask import Blueprint, request,json
 from ..utils import get_distance
 recommend_blue = Blueprint('recommend_blue', __name__)
 
+
+# 得到用户与一组商家的距离
+def get_distance_by_location(user_location, business_location_list):
+    distances = []
+    for business_location in business_location_list:
+        distance = get_distance(user_location, business_location)
+        distances.append(distance)
+    return distances
+
+
+
+
 @recommend_blue.route('/')
 def get_recommendations():
     position = json.loads(request.args.get('position'))
