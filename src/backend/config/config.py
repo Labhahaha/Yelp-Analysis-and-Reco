b3be11a5f3809e4468b4e439072312dbca3982bd
电子笨蛋 <103497254+electronic-pig@users.noreply.github.com>
2024-03-22 11:27:33 +0800

    Update config.py

--- src/backend/config/config.py
+++ src/backend/config/config.py
@@ -1,4 +1,4 @@
 # 数据库连接配置
 # 替换为你的数据库地址
-DATABASE_URL = f'mysql+pymysql://rooot:123456@localhost:3306/bigdata_yelp'
+DATABASE_URL = f'mysql+pymysql://root:123456@localhost:3306/bigdata_yelp'
 TRACK_MODIFICATIONS = False
